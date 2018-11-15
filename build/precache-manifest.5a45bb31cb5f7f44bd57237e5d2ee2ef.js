8d1d2d8c8571da066f944d021cfcb5d7f9b86f11
James <jdula1990@gmail.com>
2018-11-15 16:27:13 -0600

    build file allowed

---
+++ build/precache-manifest.5a45bb31cb5f7f44bd57237e5d2ee2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1a734cab99719133ed9",
+    "url": "/static/js/main.d1a734ca.chunk.js"
+  },
+  {
+    "revision": "68372edae1673a4980bd",
+    "url": "/static/js/1.68372eda.chunk.js"
+  },
+  {
+    "revision": "d1a734cab99719133ed9",
+    "url": "/static/css/main.dc46db3f.chunk.css"
+  },
+  {
+    "revision": "68372edae1673a4980bd",
+    "url": "/static/css/1.c9b66624.chunk.css"
+  },
+  {
+    "revision": "623e3fb16c484530813218dfd51ccbf3",
+    "url": "/index.html"
+  }
+];
